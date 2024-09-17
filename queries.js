b914187bc3eb18d746df0f371b17a604e768c270
satish.thapa <satish.thapa@innovatetech.co>
2024-09-17 16:47:13 +0545

    added question_text

--- queries.js
+++ queries.js
@@ -3,7 +3,7 @@ import { BATCH_SIZE } from "./common.js"
 //13361
 export async function fetchBucketQuestions(offset = 0, pgClient) {
   try {
-    const query = `select q.id ,q.question,q.client_id from questions q WHERE q.deleted_at IS null and q.is_custom_question = 'false'
+    const query = `select q.id ,q.question,q.client_id,q.question_text from questions q WHERE q.deleted_at IS null and q.is_custom_question = 'false'
                     LIMIT $1 OFFSET $2`
     const res = await pgClient.query(query, [BATCH_SIZE, offset])
     return res.rows
