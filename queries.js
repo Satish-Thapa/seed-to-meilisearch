86d84614de0aeb2034cc76d72ed21ddbc0a9dba7
satish.thapa <satish.thapa@innovatetech.co>
2025-01-06 12:09:37 +0545

    fixed insert by aadding order by

--- queries.js
+++ queries.js
@@ -3,8 +3,8 @@ import { BATCH_SIZE } from "./common.js"
 //13361
 export async function fetchBucketQuestions(offset = 0, pgClient) {
   try {
-    const query = `select q.id ,q.client_id,q.question_text, q.reference  from questions q WHERE q.deleted_at IS null and q.is_custom_question = 'false'
-                    LIMIT $1 OFFSET $2`
+    const query = `select q.id ,q.client_id,q.question_text, q.reference  from questions q WHERE q.deleted_at IS null and (q.is_custom_question = 'false' or q.is_custom_question is null)
+                    order by q.created_at LIMIT $1 OFFSET $2`
     const res = await pgClient.query(query, [BATCH_SIZE, offset])
     return res.rows
   } catch (err) {
